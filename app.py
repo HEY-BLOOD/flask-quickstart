2366b50511da85b2af88d0d56b5c61590156fc81
blood <blood1740@aliyun.com>
2020-10-20 11:31:33 +0800

    变量规则

--- app.py
+++ app.py
@@ -1,11 +1,44 @@
-from flask import Flask
+from flask import Flask, escape
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return 'Index Page'
+
+
+# 使用 route() 装饰器来把函数绑定到 URL
+@app.route('/hello')
+def hello():
+    return 'Hello, World！'
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    """
+    不指定转换器（缺省值） 接受任何不包含斜杠的文本
+    """
+    # show the user profile for that user
+    return 'User %s' % escape(username)
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    """
+    使用转换器，为变量指定规则为 int类型（正整数）
+    """
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    """
+    使用转换器，为变量指定规则为 path类型（类似 string ，但可以包含斜杠）
+    """
+    # show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)
 
 
 if __name__ == "__main__":
